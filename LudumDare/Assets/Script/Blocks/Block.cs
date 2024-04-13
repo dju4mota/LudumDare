ccc7dc204d9f7bf0cf101e5d944a3126d29c27c4
legolidia <fernando.b@ges.inatel.br>
2024-04-13 02:26:03 -0300

    cooldown block

--- LudumDare/Assets/Script/Blocks/Block.cs
+++ LudumDare/Assets/Script/Blocks/Block.cs
@@ -1,30 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
 
     private Transform poosition;
-    private int duration;
+    private float duration;
     private int id;
+
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            LooseDuration();
+            Invoke("CooldownPassed", duration);
         }
     }
 
-    private void LooseDuration()
+    private void CooldownPassed()
     {
-        duration--;
-        if(duration <= 0)
-        {
             Destroy(this,0.3f);
-        }
     }
 
 }
