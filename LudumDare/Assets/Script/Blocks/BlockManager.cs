bf8a79f8b2baf60e8bbfa2a6349f9d7b5192cf65
legolidia <fernando.b@ges.inatel.br>
2024-04-12 23:27:06 -0300

    blocks

---
+++ LudumDare/Assets/Script/Blocks/BlockManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    [SerializeField] List<Block> blockList;
+
+    private void Start()
+    {
+        
+    }
+
+    public void addBlock()
+    {
+
+    }
+
+    public void removeBlock()
+    {
+
+    }
+}
--- LudumDare/Assets/Script/Blocks/Slide.cs
+++ LudumDare/Assets/Script/Blocks/Slide.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloco : MonoBehaviour
+public class Slide : Block
 {
     // Start is called before the first frame update
     void Start()
