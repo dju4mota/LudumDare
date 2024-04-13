d4f38fd80f27a83e389d8f804a66dd2f80acae80
legolidia <fernando.b@ges.inatel.br>
2024-04-12 23:49:24 -0300

    block

--- LudumDare/Assets/Script/Blocks/Block.cs
+++ LudumDare/Assets/Script/Blocks/Block.cs
@@ -9,4 +9,22 @@ public class Block : MonoBehaviour
     private int duration;
     private int id;
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            LooseDuration();
+        }
+    }
+
+    private void LooseDuration()
+    {
+        duration--;
+        if(duration <= 0)
+        {
+            Destroy(this,0.3f);
+        }
+    }
+
 }
--- LudumDare/Assets/Script/Blocks/BlockManager.cs
+++ LudumDare/Assets/Script/Blocks/BlockManager.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class BlockManager : MonoBehaviour
 {
-    [SerializeField] List<Block> blockList;
+    //[SerializeField] private List<GameObject> blockList;
+    [SerializeField] private GameObject block;
+    [SerializeField] private GameObject slide;
+    [SerializeField] private GameObject timer;
+    [SerializeField] private GameObject bounce;
+
+
+    public enum BlockEnum
+    {
+        Block,
+        Slide,
+        Bounce,
+        Timer
+
+    }
+
 
     private void Start()
     {
         
     }
 
-    public void addBlock()
-    {
 
+    public void addBlock(BlockEnum blockType, Vector3 pos)
+    {
+        switch (blockType)
+        {
+            case BlockEnum.Block:
+                Instantiate(block, pos, Quaternion.identity);
+                //GameObject obj = Instantiate(block, pos, Quaternion.identity);
+                //blockList.Add(obj);
+                break;
+            case BlockEnum.Bounce:
+                Instantiate(bounce, pos, Quaternion.identity);
+                break;
+            case BlockEnum.Slide:
+                Instantiate(slide, pos, Quaternion.identity);
+                break;
+            case BlockEnum.Timer:
+                Instantiate(timer, pos, Quaternion.identity);
+                break;
+        }
     }
+
 
     public void removeBlock()
     {
