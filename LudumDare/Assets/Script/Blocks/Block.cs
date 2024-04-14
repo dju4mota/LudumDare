47731a50aecf35bb9c455ed4ad5fe15910e2d310
Felipe Conrado <fconrado2003@gmail.com>
2024-04-14 18:23:45 -0300

    AAAAAAA

--- LudumDare/Assets/Script/Blocks/Block.cs
+++ LudumDare/Assets/Script/Blocks/Block.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-
-    private Transform poosition;
+    private AnimationController anim;
     [SerializeField] private float duration = 2f;
-    private int id;
+    private float timer;
+
+    private void Start(){
+        anim = GetComponent<AnimationController>();
+        timer = duration;
+    }
+
+    private void Update(){
+        timer -= Time.deltaTime;
+        if(timer > 2*duration/3){
+            anim.ChangeAnimationState("1");
+        }
+        if(timer == 2*duration/3){
+            anim.ChangeAnimationState("2");
+        }
+        else if(timer == duration/3){
+            anim.ChangeAnimationState("3");
+        }
+    }
 
 
 
