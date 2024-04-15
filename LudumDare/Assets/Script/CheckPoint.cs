f08b3ed62f4d2e0d329e109e8c558fcdbfd6b9c3
Felipe Conrado <fconrado2003@gmail.com>
2024-04-15 12:47:27 -0300

    final assets

--- LudumDare/Assets/Script/CheckPoint.cs
+++ LudumDare/Assets/Script/CheckPoint.cs
@@ -7,37 +7,28 @@ public class CheckPoint : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     [SerializeField] Sprite[] sprite;
-    private int i = 0;
+    bool isCharged = false;
 
     private void Start(){
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = sprite[0];
     }
-
-    private void Update(){
-        spriteRenderer.sprite = sprite[i];
-    }
     public void OnTriggerEnter2D(Collider2D col){
-        if(col.gameObject.CompareTag("Player")){
-            StopCoroutine(Unload());
+        if(col.gameObject.CompareTag("Player") && isCharged == false){
             StartCoroutine(Load());
         }
     }
 
     public void OnTriggerExit2D(Collider2D col){
-         if(col.gameObject.CompareTag("Player")){
-            StopCoroutine(Load());
-            StartCoroutine(Unload());
-        }
+        StopCoroutine(Load());
     }   
 
     public IEnumerator Load(){
-        i++;
+        for(int i = 0; i < sprite.Length -1; i++){
+             spriteRenderer.sprite = sprite[i];
         yield return new WaitForSeconds(0.2f);
-    }
-
-    public IEnumerator Unload(){
-        i--;
-        yield return new WaitForSeconds(0.2f);
+        }
+        isCharged = true;
+        GameManager.Instance.checkpoint = transform;
     }
 }
